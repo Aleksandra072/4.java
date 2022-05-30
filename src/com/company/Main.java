4363b7e652382d33c5bf2d4f79ce7b54077f314f
Aleksandra072 <konopacka.ola@wp.pl>
2022-05-30 16:59:07 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String [] imiona = {"Piotr","Anna","Jan","Leszek"};
+
+        int i;
+        for(i = 0; i < imiona.length; i++){
+            System.out.println(i + 1+ "." +imiona[i]);
+        }
+        System.out.println();
+    }
+}
